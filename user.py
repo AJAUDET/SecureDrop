a44e59508626594b76cbc790591165291a3a986e
Arthur Audet <150381581+AJAUDET@users.noreply.github.com>
2025-10-21 22:12:18 +0000

    ...when i initially created the add user function it could only read one user, i fixed it to read multiple

--- user.py
+++ user.py
@@ -4,21 +4,42 @@
 
 import json
 import sys
+import os
 from password import create_salted_hash
 
-def add_user(outFile): 
+def add_user(outFile):
+    user = input("Enter a Username: ")
+    pwd = input("Enter a Password: ")
     try:
-        with open(outFile, 'x') as outF:
-            user = input("Enter a Username: ")
-            pswd = input("Enter a Password: ")
-            data = {"User: ": user, "Password: ": create_salted_hash(pswd)}
-            print(json.dumps(data, separators=(',', ':')), file=outF)
-    except:
-        with open(outFile, 'a+') as outF:
-            user = input("Enter a Username: ")
-            pswd = input("Enter a Password: ")
-            data = {"User: ": user, "Password: ": create_salted_hash(pswd)}
-            print(json.dumps(data, separators=(',', ':')), file=outF)
+        if os.path.exists(outFile):
+            with open(outFile, 'r') as outF:
+                data = json.load(outF)
+                if "User" in data:
+                    old_usr = data["User"]
+                    old_pwd = data["Password"]
+                    data = {
+                        "Users": {
+                            old_usr : old_pwd
+                        }
+                    }
+        else:
+            data = {"Users": {}}
+
+        if user in data["Users"]:
+            print(f"User already registered")
+            return
+
+        pwd_hash = create_salted_hash(pwd)
+        data["Users"][user] = pwd_hash
+        with open(outFile, 'w') as outF:
+            json.dump(data, outF, indent=2)
+    except json.JSONDecodeError:
+        print("Error: Corrupted database file")
+    except PermissionError:
+        print("Error: No permission to write to file")
+    except Exception as e:
+        print(f"Error adding user: {type(e).__name__}")
+          
 
 if __name__ == "__main__":
     if (len(sys.argv) != 3):
