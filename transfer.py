d884940dc3f9269bd0a9469ddf0fc09ad3c584cf
Arthur Audet <150381581+AJAUDET@users.noreply.github.com>
2025-10-21 17:37:29 +0000

    transfer.py is a test to see how file transfer works

--- transfer.py
+++ transfer.py
@@ -1,4 +1,5 @@
 # Author : AJ Audet
 # Purpose : Trial for using a Python script to transfer files over a network
+# ALT : This is mainly a test to see how transfering files over a network works
 
 import html
