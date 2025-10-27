96c2dc2fefa797b3eac1c5f85d54e9338f30083f
Arthur Audet <150381581+AJAUDET@users.noreply.github.com>
2025-10-27 23:19:03 +0000

    How we will welcome users, add to this later

---
+++ welcome.py
@@ -0,0 +1,8 @@
+
+
+def welcome_msg(username):
+    print("------------------------------------------------------")
+    print("\n\n\t\tLog in successful\t\t\n\n")
+    print("------------------------------------------------------")
+    print(f"\tHello {username} Welcome to Secure Drop\t\t")
+    print("\tThis program is designed to transfer files between you and one of your contacts, given that they are also online")
