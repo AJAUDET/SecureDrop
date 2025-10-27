bc6b04083b2c28b430bc331ed080ce006053b784
Arthur Audet <150381581+AJAUDET@users.noreply.github.com>
2025-10-27 19:44:08 +0000

    Removed redundancy in listing commands by removing helpu function

--- commands.py
+++ commands.py
@@ -4,14 +4,5 @@ import contacts
 def add():
     contacts.add_contact()
 
-def helpu():
-    print("Available Commands:")
-    print("add   -> Add a new contact")
-    print("list  -> List all online contacts")
-    print("send  -> Transfer file to contact")
-    print("verify -> Verify a contact's identity")
-    print("exit  -> Exit SecureDrop")
-
 def listc():
     contacts.list_contacts()
-
--- securedrop.py
+++ securedrop.py
@@ -1,6 +1,7 @@
 import verify
 import pwinput
 import user
+import os
 from contactmanage import add_contact, list_contacts, verify_contact, admin_list, admin_clear
 from network import start_network
 import sys
@@ -11,6 +12,7 @@ command_map = {
     "verify": verify_contact,
     "admin_list": admin_list,
     "admin_clear": admin_clear,
+    "clear": lambda clear: os.system('clear'),
     "exit": lambda username: sys.exit(0)
 }
 
@@ -18,7 +20,7 @@ def main(username):
     print(f"Welcome to Secure Drop, {username}!")
     print("Enter 'help' for a list of commands, when done enter 'exit'")
     while True:
-        cmd = input("$: ").strip()
+        cmd = input(f"{username}: ").strip()
         if cmd in command_map:
             command_map[cmd](username)  
         elif cmd == "help":
@@ -26,9 +28,11 @@ def main(username):
             print("add - Add a new contact")
             print("list - List all contacts")
             print("verify - Verify a contact's identity")
-            print("admin_list - View all users’ contacts (admin only)")
-            print("admin_clear - Clear the master contact log (admin only)")
+            print("clear - Clears the terminal of previous commands")
             print("exit - Exit SecureDrop")
+            if username == 'admin':
+                print("admin_list - View all users’ contacts (admin only)")
+                print("admin_clear - Clear the master contact log (admin only)")
         else:
             print("Unknown command. Type 'help' for a list of commands.")
 
