f8658385f9d5eff9735e9c4ecfb0d1678e700c95
Arthur Audet <150381581+AJAUDET@users.noreply.github.com>
2025-10-28 00:02:12 +0000

    Added current date and time to login message

--- welcome.py
+++ welcome.py
@@ -1,8 +1,11 @@
-
+from datetime import datetime
+import pytz
 
 def welcome_msg(username):
     print("------------------------------------------------------")
     print("\n\n\t\tLog in successful\t\t\n\n")
     print("------------------------------------------------------")
     print(f"\tHello {username} Welcome to Secure Drop\t\t")
-    print("\tThis program is designed to transfer files between you and one of your contacts, given that they are also online")
+    print("\tThis program is designed to transfer files between you and your contacts, given that they are also online\n\n")
+    print(f"\tLogin Date: {datetime.today().strftime("%d/%m/%Y")}")
+    print(f"\tLogin Time: {datetime.now(pytz.utc).astimezone(pytz.timezone('US/Eastern')).strftime("%H:%M:%S")}")
