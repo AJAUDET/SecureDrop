d57049b75f4e6fb3d95e248deab23e4f1d2990f3
Lourenco DaSilva <ldasilva@Lourencos-MacBook-Pro.local>
2025-10-28 16:38:02 -0400

    fixed text issues

--- welcome.py
+++ welcome.py
@@ -2,10 +2,14 @@ from datetime import datetime
 import pytz
 
 def welcome_msg(username):
+    print("\n\n")
     print("------------------------------------------------------")
     print("\n\n\t\tLog in successful\t\t\n\n")
     print("------------------------------------------------------")
     print(f"\tHello {username} Welcome to Secure Drop\t\t")
-    print("\tThis program is designed to transfer files between you and your contacts, given that they are also online\n\n")
-    print(f"\tLogin Date: {datetime.today().strftime("%d/%m/%Y")}")
-    print(f"\tLogin Time: {datetime.now(pytz.utc).astimezone(pytz.timezone('US/Eastern')).strftime("%H:%M:%S")}")
+    print("This program is designed to transfer files between you and your contacts, given that they are also online\n\n")
+    print(f"\tLogin Date: {datetime.today().strftime('%d/%m/%Y')}")
+    print(f"\tLogin Time: {datetime.now(pytz.utc).astimezone(pytz.timezone('US/Eastern')).strftime('%H:%M:%S')}")
+    print(f"\n\n\tHave a secure day, {username}!")
+    print(f"Enter 'help' to see a list of available commands.")
+    print("------------------------------------------------------")
